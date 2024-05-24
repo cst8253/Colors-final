9223ce733af800c8eb630435c2674fbff4265f75
Michael Eisenbraun <eisenbm@algonquincollege.com>
2024-05-24 08:50:02 -0400

    Creates final version

--- Program.cs
+++ Program.cs
@@ -148,3 +148,121 @@
   "gray",
   "palegoldenrod"
 ];
+
+// Retrieve a color
+// Console.WriteLine(colors[3]); // slateblue
+
+// Add a color
+// colors.Add("bluegreen");
+
+// Sort colors
+// colors.Sort();
+
+// Remove a color
+// colors.Remove("aliceblue");
+
+// Display colors
+// Console.WriteLine();
+// Console.WriteLine(string.Join("\n", colors));
+
+// Display colors in alpha order
+void Show (List<string> colors) 
+{
+  colors.Sort();
+  Console.WriteLine();
+  Console.WriteLine(string.Join("\n", colors));
+}
+
+// Add a color requested by user
+void Add ()
+{
+  Console.WriteLine("\nEnter the color would you like to add:");
+  string? color = Console.ReadLine();
+  if (!string.IsNullOrEmpty(color))
+  {
+    if (colors.Contains(color))
+    {
+      Console.WriteLine($"\n{color} has already been added to the list");
+    }
+    else
+    {
+      colors.Add(color);
+      Console.WriteLine($"\n{color} has been added to the list");
+    }
+  }
+}
+
+// remove color requested by user
+void Remove ()
+{
+  Console.WriteLine("\nEnter the color would you like to remove:");
+  string? color = Console.ReadLine();
+  if (!string.IsNullOrEmpty(color))
+  {
+    if (!colors.Contains(color))
+    {
+      Console.WriteLine($"\n{color} could not be found in the list.");
+    }
+    else
+    {
+      colors.Remove(color);
+      Console.WriteLine($"\n{color} has been remove from the list");
+    }
+  }
+}
+
+// search colors based on user-provided terms
+void Search () 
+{
+  Console.WriteLine("\nEnter your search term:");
+  string? query = Console.ReadLine();
+
+  if (!string.IsNullOrEmpty(query))
+  {
+    var filtered = colors.FindAll(color => color.ToLower().Contains(query.ToLower()));
+    if (filtered.Count > 0) 
+    {
+      Show(filtered);
+    }
+    else
+    {
+      Console.WriteLine("\nNo colors matched the search term.");
+    }
+  }
+}
+
+bool isActive = true;
+
+while(isActive)
+{
+  Console.WriteLine("\nChoose a command:");
+  Console.WriteLine("1 - Show all colors");
+  Console.WriteLine("2 - Add a color");
+  Console.WriteLine("3 - Remove a color");
+  Console.WriteLine("4 - Search colors");
+  Console.WriteLine("5 - Exit");
+
+  bool isValid = int.TryParse(Console.ReadLine(), out int command);
+
+  switch (command)
+  {
+    case 1:
+      Show(colors);
+      break;
+    case 2:
+      Add();
+      break;
+    case 3:
+      Remove();
+      break;
+    case 4:
+      Search();
+      break;
+    case 5:
+      isActive = false;
+      break;
+    default:
+      Console.WriteLine("Not a valid command.");
+      break;
+  }
+}
